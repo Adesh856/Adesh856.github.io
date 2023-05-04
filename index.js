347ca22d3eab30fc8017b3de5032d4a4e4de9a48
Adesh856 <adeshtayde6@gmail.com>
2023-05-04 23:49:45 +0530

    add

--- index.js
+++ index.js
@@ -41,13 +41,13 @@ GitHubCalendar(".calendar", "Adesh856", {
   tooltips: true,
 });
 
-document.getElementById("resume-link-1").onclick = () => {
+document.getElementById("resume-button-1").onclick = () => {
   window.open(
     "https://drive.google.com/file/d/1bHUhuBBaZXtbrab9mh3z1ZGXCxdL2RZ9/view?usp=sharing"
   );
 };
 
-document.getElementById("resume-link-2").onclick = () => {
+document.getElementById("resume-button-2").onclick = () => {
   window.open(
     "https://drive.google.com/file/d/1bHUhuBBaZXtbrab9mh3z1ZGXCxdL2RZ9/view?usp=sharing"
   );
